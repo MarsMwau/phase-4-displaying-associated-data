5223e7486f102b8b36d0ea7ff185cf17f5b9cfad
MarsMwau <marthamumbua16@gmail.com>
2023-06-22 00:19:09 +0300

    Completed Lab

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -3,8 +3,10 @@ class DogHousesController < ApplicationController
 
   def show
     dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    render json: dog_house, include: :reviews
   end
+
+  
 
   private
 
